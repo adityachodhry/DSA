11a8bc7a4be9c417eb4d5594fa5025ad0ff2a91b
Aditya Choudhary <adityachy02131@gmail.com>
2024-08-06 23:46:26 +0530

    Day 1 bigOh Notation

--- python_DSA/bigOh.py
+++ python_DSA/bigOh.py
@@ -1,23 +1,29 @@
 import time
 
+# start = time.time()
+# i=1
+# while i<2:
+#     print(i)
+#     i+=1
+# print(time.time()-start)
+
+# x = [1,2,3,4,5,6]
+# sum = 0
+# # For first loop
+# for i in x:
+#     sum = sum+i
+# print(sum)
+# print(time.time()-start)
+
+# # For Second loop   
+# product = 1
+# for i in x:
+#     product = product*i
+# print(product)
+# print(time.time()-start)
+
 start = time.time()
-i=1
-while i<2:
+for i in range(1, 101):
     print(i)
-    i+=1
-print(time.time()-start)
 
-x = [1,2,3,4,5,6]
-sum = 0
-# For first loop
-for i in x:
-    sum = sum+i
-print(sum)
-print(time.time()-start)
-
-# For Second loop
-product = 1
-for i in x:
-    product = product*i
-print(product)
 print(time.time()-start)
